d11ffa60e6ec5fef07116d0c863ee7da6671cc0b
Dimitris Vavatsioulas <dvavatsioulas@gmail.com>
2019-07-23 12:28:04 +0300

    Add Bounding Box

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import './App.css';
 
 const app = new Clarifai.App({
   apiKey: 'b4057d386e904840aba8e11228071a45'
- });
- 
+});
+
 const particleOptions = {
   particles: {
     number: {
@@ -32,24 +32,40 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box })
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   }
 
   onSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.input).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
+
+
   render() {
     return (
       <div className="App">
@@ -57,7 +73,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onSubmit }) => {
     return (
         <div>
+            <a href="https://www.github.com/dvavatsioulas/"><h3 className='near-black' >created by @dvavatsioulas</h3></a>
             <p className='f3'>
+
                 {'FatsaDetector detects faces in your images.'}
                 <br></br>
                 {'Just paste the link below and click Detect!'}
             </p>
+
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
